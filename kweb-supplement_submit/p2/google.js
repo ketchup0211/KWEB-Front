0c67e2cefbfb6629a663a598947b9855e00fb7e0
cham <dlcka971711@gmail.com>
2023-08-28 01:11:37 +0900

    [END]kewb-supplement_p2

---
+++ kweb-supplement_submit/p2/google.js
@@ -0,0 +1,10 @@
+function goGitHub(){
+    open('https://github.com/ketchup0211');
+}
+
+function goDoodles(){
+    open('https://www.google.com/doodles');
+}
+
+document.getElementById('cham-github').addEventListener('click', goGitHub);
+document.getElementById('doodles').addEventListener('click', goDoodles);
